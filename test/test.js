fa1b09a36b7ef88b52cb5bf56798bb10ef159339
Nathan <nathan@users.noreply.github.com>
2018-08-23 14:46:10 -0400

    Test default tokens

--- test/test.js
+++ test/test.js
@@ -181,6 +181,116 @@ describe('compiles literals', () => {
 
 })
 
+describe('default tokens', () => {
+
+  test('work', () => {
+    const lexer = moo.compile({
+      op: /[._]/,
+      text: moo.default,
+    })
+    lexer.reset('.this_that.')
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'this'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '_'})
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'that'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+  })
+
+  test(`work if there are characters before the first token`, () => {
+    const lexer = compile({
+      op: /[._]/,
+      text: moo.default,
+    })
+    lexer.reset('.stuff')
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'stuff'})
+  })
+
+  test(`work if there are characters after the last token`, () => {
+    const lexer = compile({
+      op: /[._]/,
+      text: moo.default,
+    })
+    lexer.reset('stuff.')
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'stuff'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+  })
+
+  test('work on stateful lexers', () => {
+    const lexer = moo.states({
+      main: {
+        op: /[._]/,
+        switch: {match: '|', next: 'other'},
+        text: moo.default,
+      },
+      other: {
+        op: /[+-]/,
+      },
+    })
+    lexer.reset('foo.bar_baz|++-!')
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'foo'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'bar'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '_'})
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'baz'})
+    expect(lexer.next()).toMatchObject({type: 'switch', value: '|'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '+'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '+'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '-'})
+    expect(() => lexer.next()).toThrow('invalid syntax')
+  })
+
+  test(`are never empty`, () => {
+    const lexer = moo.compile({
+      op: /[._]/,
+      text: moo.default,
+    })
+    lexer.reset('.._._')
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '_'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '.'})
+    expect(lexer.next()).toMatchObject({type: 'op', value: '_'})
+  })
+
+  test(`report token positions correctly`, () => {
+    const lexer = moo.compile({
+      op: /[._]/,
+      text: moo.default,
+    })
+    lexer.reset('.this_th\nat.')
+    expect(lexer.next()).toMatchObject({value: '.', offset: 0})
+    expect(lexer.next()).toMatchObject({value: 'this', offset: 1})
+    expect(lexer.next()).toMatchObject({value: '_', offset: 5})
+    expect(lexer.next()).toMatchObject({value: 'th\nat', offset: 6})
+    expect(lexer.next()).toMatchObject({value: '.', offset: 11})
+  })
+
+  test(`report token line numbers correctly`, () => {
+    const lexer = moo.compile({
+      str: {lineBreaks: true, match: /"[^]+?"/},
+      bare: moo.default,
+    })
+    lexer.reset('a\nb"some\nthing" else\ngoes\nhere\n\n"\nand here"\n')
+    expect(lexer.next()).toMatchObject({value: 'a\nb', line: 1, col: 1})
+    expect(lexer.next()).toMatchObject({value: '"some\nthing"', line: 2, col: 2})
+    expect(lexer.next()).toMatchObject({value: ' else\ngoes\nhere\n\n', line: 3, col: 7})
+    expect(lexer.next()).toMatchObject({value: '"\nand here"', line: 7, col: 1})
+    expect(lexer.next()).toMatchObject({value: '\n', line: 8, col: 10})
+  })
+
+  test("don't throw token errors until next() is called again", () => {
+    const lexer = moo.compile({
+      op: {match: /[._]/, shouldThrow: true},
+      text: moo.default,
+    })
+    lexer.reset('stuff.')
+    expect(lexer.next()).toMatchObject({type: 'text', value: 'stuff'})
+    expect(() => lexer.next()).toThrow('invalid syntax')
+  })
+
+})
+
 describe('keywords', () => {
 
   test('supports explicit keywords', () => {
